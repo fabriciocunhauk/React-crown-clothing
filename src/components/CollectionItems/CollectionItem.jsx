63131eebaa8832bc94e5d1e2db8ac00ad6e6a52e
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-10-30 02:24:26 +0000

    added Colection item add to cart button

--- src/components/CollectionItems/CollectionItem.jsx
+++ src/components/CollectionItems/CollectionItem.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import CustomButton from '../CustomButton/CustomButton';
 
 import './collectionItem.styles.scss';
 
@@ -13,6 +14,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton inverted>ADD TO CART</CustomButton>
         </div>
     )
 }
