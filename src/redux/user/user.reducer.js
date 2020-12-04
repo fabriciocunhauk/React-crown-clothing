dba966989a52b66875b4e53dd680ef4df85457a7
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-12-04 03:48:35 +0000

    Corrected typo

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,10 @@
 import { UserActionTypes } from "./user.types";
 
-const INITIOAL_STATE = {
+const INITIAL_STATE = {
     currentUser: null
 }
 
-const userReducer = (state = INITIOAL_STATE, action) => {
+const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case UserActionTypes.SET_CURRENT_USER:
             return {
