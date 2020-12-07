ae8a597688032c06199d5a43f7cec48e5e858afc
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-12-07 04:14:27 +0000

    Implemented redux-persist for persisting data on checkout page, created directory, shop reducers and shop overview componenet

--- src/pages/shopPage/ShopPage.jsx
+++ src/pages/shopPage/ShopPage.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js';
-import CollectionPreview from '../../components/CollectionPreview/CollectionPreview.jsx';
+import CollectionOverview from '../../components/CollectionOverview/CollectionOverview';
 
-class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
-
-    render() {
-        const { collections } = this.state;
-
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))
-                }
-            </div>)
-    }
-}
+const ShopPage = () => (
+    <div className="shop-page">
+        <CollectionOverview />
+    </div>
+);
 
 export default ShopPage;
