c7109f728b015acd234c4e1d93908989d1dd129e
Fabricio Cunha <fabriciocunhadeveloper@gmail.com>
2020-12-08 03:30:11 +0000

    Advanced routing

---
+++ src/pages/collection/CollectionPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import CollectionItem from '../../components/CollectionItems/CollectionItem';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ collection }) => (
+    <div className="collection-page">
+        <h2>Category page</h2>
+    </div>
+);
+
+const mapStateToProps = (state, ownProps) => ({
+    collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shopPage/ShopPage.jsx
+++ src/pages/shopPage/ShopPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
 import CollectionOverview from '../../components/CollectionOverview/CollectionOverview';
+import CollectionPage from '../collection/CollectionPage';
 
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
     <div className="shop-page">
-        <CollectionOverview />
+        <Route exact path={match.path} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
